2a430a0bf7ac960da798d782a8a1af1555209461
zhufengnodejs <zhufengnodejs@126.com>
2019-08-15 23:42:45 +0800

    msg

--- webhook.js
+++ webhook.js
@@ -2,6 +2,7 @@ let http = require('http');
 let crypto = require('crypto');
 let {spawn} = require('child_process');
 let SECRET= '123456';
+let sendMail = require('./sendMail');
 function sign(body){
     return `sha1=`+crypto.createHmac('sha1',SECRET).update(body).digest('hex');
 }
@@ -30,8 +31,14 @@ let server = http.createServer(function(req,res){
                     buffers.push(buffer);
                 });
                  child.stdout.on('end',function(buffer){
-                    let log = Buffer.concat(buffers);
-                    console.log(log);
+                    let log = Buffer.concat(buffers).toString();
+                    sendMail(`
+                        <h1>部署日期: ${new Date()}</h1>
+                        <h2>部署人: ${payload.pusher.name}</h2>
+                        <h2>部署邮箱: ${payload.pusher.email}</h2>
+                        <h2>提交信息: ${payload.head_commit&&payload.head_commit['message']}</h2>
+                        <h2>布署日志: ${logs.replace("\r\n",'<br/>')}</h2>
+                    `);
                 });
            }
        });
