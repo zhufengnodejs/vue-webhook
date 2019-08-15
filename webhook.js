5890574596f58fb5fd5eff05753bac4d282044df
zhufengnodejs <zhufengnodejs@126.com>
2019-08-15 23:02:34 +0800

    init

--- webhook.js
+++ webhook.js
@@ -1,7 +1,25 @@
 let http = require('http');
+let crypto = require('crypto');
+let SECRET= '123456';
+function sign(body){
+    return `sha1=`+crypto.creatHmac('sha1',SECRET).update(body).digest('hex');
+}
 let server = http.createServer(function(req,res){
    console.log(req.method,req.url);
    if(req.method == 'POST' && req.url == '/webhook'){
+       let buffers = [];
+       req.on('data',function(buffer){
+           buffers.push(buffer);
+       });
+       req.on('end',function(buffer){
+           let body = Buffer.concat(buffers);
+           let event = req.header['x-gitHub-event'];//event=push
+           //github请求来的时候，要传递请求体body,另外还会传一个signature过来，你需要验证签名对不对
+           let signature = req.headers['x-hub-signature'];
+           if(signature  !== sign(body)){
+             return res.end('Not Allowed');
+           }
+       });
        res.setHeader('Content-Type','application/json');
        res.end(JSON.stringify({ok:true}));
    }else{
